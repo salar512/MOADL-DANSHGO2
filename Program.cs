62831f3b02090b6265e244116d3aa6dd5a437abe
Hani <Hani@DESKTOP-Q9D08B7>
2023-10-31 21:33:45 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moadl_danshgo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//محمد سالار رحیمی معدل دانشجو
+            int rizi;
+            Console.WriteLine("rizi=");
+            rizi = Convert.ToInt32(Console.ReadLine());
+            if (rizi > 0 & rizi <= 20) ;
+
+            else
+            {
+                Console.WriteLine("rizi=");
+                rizi = Convert.ToInt32(Console.ReadLine());
+            }
+            if (rizi % 2 == 0)
+            { Console.WriteLine("zoj"); }
+            else
+            {
+                Console.WriteLine("fard");
+            }
+            int zarib1;
+            Console.WriteLine("zarib1=");
+            zarib1 = Convert.ToInt32(Console.ReadLine());
+            if (zarib1 > 0 & zarib1 <= 4) ;
+
+            else
+            {
+                Console.WriteLine("zarib1=");
+                zarib1 = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int fizik;
+            Console.WriteLine("fizik=");
+            fizik = Convert.ToInt32(Console.ReadLine());
+            if (fizik > 0 & fizik <= 20) ;
+
+            else
+            {
+                Console.WriteLine("fizik=");
+                fizik = Convert.ToInt32(Console.ReadLine());
+            }
+            if (fizik % 2 == 0)
+            { Console.WriteLine("zoj"); }
+            else
+            {
+                Console.WriteLine("fard");
+            }
+
+            int zarib2;
+            Console.WriteLine("zarib2=");
+            zarib2 = Convert.ToInt32(Console.ReadLine());
+            if (zarib2 > 0 & zarib2 <= 4) ;
+
+            else
+            {
+                Console.WriteLine("zarib2=");
+                zarib2 = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int shimi;
+            Console.WriteLine("shimi=");
+            shimi = Convert.ToInt32(Console.ReadLine());
+            if (shimi > 0 & shimi <= 20) ;
+
+            else
+            {
+                Console.WriteLine("shimi=");
+                shimi = Convert.ToInt32(Console.ReadLine());
+            }
+            if (shimi % 2 == 0)
+            { Console.WriteLine("zoj"); }
+            else
+            {
+                Console.WriteLine("fard");
+            }
+
+            int zarib3;
+            Console.WriteLine("zarib3=");
+            zarib3 = Convert.ToInt32(Console.ReadLine());
+            if (zarib3 > 0 & zarib3 <= 4) ;
+
+            else
+            {
+                Console.WriteLine("zarib3=");
+                zarib3 = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int hendse;
+            Console.WriteLine("hendse=");
+            hendse = Convert.ToInt32(Console.ReadLine());
+            if (hendse > 0 & hendse <= 20) ;
+
+            else
+            {
+                Console.WriteLine("hendse=");
+                hendse = Convert.ToInt32(Console.ReadLine());
+            }
+            if (hendse % 2 == 0)
+            { Console.WriteLine("zoj"); }
+            else
+            {
+                Console.WriteLine("fard");
+            }
+
+            int zarib4;
+            Console.WriteLine("zarib4=");
+            zarib4 = Convert.ToInt32(Console.ReadLine());
+            if (zarib4 > 0 & zarib4 <= 4) ;
+
+            else
+            {
+                Console.WriteLine("zarib4=");
+                zarib4 = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int amar;
+            Console.WriteLine("amar=");
+            amar = Convert.ToInt32(Console.ReadLine());
+            if (amar > 0 & amar <= 20) ;
+
+            else
+            {
+                Console.WriteLine("amar=");
+                amar = Convert.ToInt32(Console.ReadLine());
+            }
+            if (amar % 2 == 0)
+            { Console.WriteLine("zoj"); }
+            else
+            {
+                Console.WriteLine("fard");
+            }
+
+            int zarib5;
+            Console.WriteLine("zarib5=");
+            zarib5 = Convert.ToInt32(Console.ReadLine());
+            if (zarib5 > 0 & zarib5 <= 4) ;
+
+            else
+            {
+                Console.WriteLine("zarib5=");
+                zarib5 = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int tarikh;
+            Console.WriteLine("tarikh=");
+            tarikh = Convert.ToInt32(Console.ReadLine());
+            if (tarikh > 0 & tarikh <= 20) ;
+
+            else
+            {
+                Console.WriteLine("tarikh=");
+                tarikh = Convert.ToInt32(Console.ReadLine());
+            }
+            if (tarikh % 2 == 0)
+            { Console.WriteLine("zoj"); }
+            else
+            {
+                Console.WriteLine("fard");
+            }
+
+            int zarib6;
+            Console.WriteLine("zarib6=");
+            zarib6 = Convert.ToInt32(Console.ReadLine());
+            if (zarib6 > 0 & zarib6 <= 4) ;
+
+            else
+            {
+                Console.WriteLine("zarib6=");
+                zarib6 = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            int zamin;
+            Console.WriteLine("zamin=");
+            zamin = Convert.ToInt32(Console.ReadLine());
+            if (zamin > 0 & zamin <= 20) ;
+
+            else
+            {
+                Console.WriteLine("zamin=");
+                hendse = Convert.ToInt32(Console.ReadLine());
+            }
+            if (zamin % 2 == 0)
+            { Console.WriteLine("zoj"); }
+            else
+            {
+                Console.WriteLine("fard");
+            }
+
+            int zarib7;
+            Console.WriteLine("zarib7=");
+            zarib7 = Convert.ToInt32(Console.ReadLine());
+            if (zarib7 > 0 & zarib7 <= 4) ;
+
+            else
+            {
+                Console.WriteLine("zarib7=");
+                zarib7 = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int maref;
+            Console.WriteLine("maref=");
+            maref = Convert.ToInt32(Console.ReadLine());
+            if (maref > 0 & maref <= 20) ;
+
+            else
+            {
+                Console.WriteLine("maref=");
+                hendse = Convert.ToInt32(Console.ReadLine());
+            }
+            if (maref % 2 == 0)
+            { Console.WriteLine("zoj"); }
+            else
+            {
+                Console.WriteLine("fard");
+            }
+
+            int zarib8;
+            Console.WriteLine("zarib8=");
+            zarib8 = Convert.ToInt32(Console.ReadLine());
+            if (zarib8 > 0 & zarib8 <= 4) ;
+
+            else
+            {
+                Console.WriteLine("zarib8=");
+                zarib8 = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int arabi;
+            Console.WriteLine("arabi=");
+            arabi = Convert.ToInt32(Console.ReadLine());
+            if (arabi > 0 & arabi <= 20) ;
+
+            else
+            {
+                Console.WriteLine("arabi=");
+                arabi = Convert.ToInt32(Console.ReadLine());
+            }
+            if (arabi % 2 == 0)
+            { Console.WriteLine("zoj"); }
+            else
+            {
+                Console.WriteLine("fard");
+            }
+            int zarib9;
+            Console.WriteLine("zarib9=");
+            zarib9 = Convert.ToInt32(Console.ReadLine());
+            if (zarib9 > 0 & zarib9 <= 4) ;
+
+            else
+            {
+                Console.WriteLine("zarib9=");
+                zarib9 = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            int farsi;
+            Console.WriteLine("farsi=");
+            farsi = Convert.ToInt32(Console.ReadLine());
+            if (farsi > 0 & farsi <= 20) ;
+
+            else
+            {
+                Console.WriteLine("farsi=");
+                farsi = Convert.ToInt32(Console.ReadLine());
+            }
+            if (farsi % 2 == 0)
+            { Console.WriteLine("zoj"); }
+            else
+            {
+                Console.WriteLine("fard");
+            }
+            int zarib10;
+                Console.WriteLine("zarib10=");
+                zarib10 = Convert.ToInt32(Console.ReadLine());
+                if (zarib10 > 0 & zarib10 <= 4) ;
+
+                else
+                {
+                    Console.WriteLine("zarib10=");
+                    zarib10 = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine("moadl=");
+                int moadl1 = (rizi * zarib1) + (fizik * zarib2) + (shimi * zarib3) + (hendse * zarib4) + (amar * zarib5) + (tarikh * zarib6) + (zamin * zarib7) + (maref * zarib8) + (arabi * zarib9) + (farsi * zarib10);
+                int moadl2 = zarib1 + zarib2 + zarib3 + zarib4 + zarib5 + zarib6 + zarib7 + zarib8 + zarib9 + zarib10;
+              float moadl = moadl1 / moadl2;
+                Console.Write(moadl);
+                Console.ReadKey();
+            }
+        }
+    }
+
